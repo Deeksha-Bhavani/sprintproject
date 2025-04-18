933de88ee9d2102b060e68989e02722cfc92b5f8
Deeksha-Bhavani <bhavanibhani626@gmail.com>
2025-04-18 12:58:04 +0530

    Add files via upload

---
+++ T080100630_Deeksha Bhavani S_Placement Management_StudentService/Student1/src/main/java/student/service/placement/Student1Application.java
@@ -0,0 +1,13 @@
+package student.service.placement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Student1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Student1Application.class, args);
+	}
+
+}
